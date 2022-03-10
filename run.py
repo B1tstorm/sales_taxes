1b82d3db957b424cf46e9d38d67451a2b1b189c9
kamil.o.gorczyca <kamil.o.gorczyca@student.fh-kiel.de>
2022-03-10 21:34:46 +0100

    Input functionality with check if user want to pay

--- run.py
+++ run.py
@@ -1,16 +1,22 @@
 # Author: Oliver Gorczyca
 # Date: 9.3.22
+from sales_taxes.ProductIO import ProductIO
 from sales_taxes.Receipt import Receipt
 
 
 def main():
-    print("hello world!")
-    test_receipt = Receipt
-    receipt = Receipt()
-    print(test_receipt)
-    print(receipt)
-    if isinstance(test_receipt, Receipt):
-        print("Its not null")
+    io = ProductIO()
+
+    while True:
+        product_string = io.product_input()
+        if check_for_checkout(product_string):
+            break
+
+
+def check_for_checkout(user_input):
+    if user_input == "checkout":
+        # TODO: - print receipt
+        return True
 
 
 if __name__ == "__main__":
--- sales_taxes/ProductIO.py
+++ sales_taxes/ProductIO.py
@@ -6,9 +6,20 @@ class ProductIO:
         self.__user_input = ""
         self.__shoppingCart = ShoppingCart
 
+        # TODO: delete after implementing class Product
+        self.product_amount = 0
+        self.imported = False
+        self.product_name = ""
+        self.product_price = 0.0
+        self.TO_LONG_INPUT = 80
+        self.TO_SHORT_INPUT = 7
+
     @staticmethod
     def product_input():
-        pass
+        try:
+            return input("\nAdd product to shoppingcart or pay with command 'checkout'\n")
+        except OverflowError as e:
+            print("Error: Invalid argument: {}".format(e))
 
     def __is_input_valid(self, user_input):
         return ""
