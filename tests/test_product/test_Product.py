bd1ee4ef70fd3ef5a9a127c99500023dcd254170
kamil.o.gorczyca <kamil.o.gorczyca@student.fh-kiel.de>
2022-03-10 14:37:53 +0100

    added more assertions to test_create_product_from_input()

--- tests/test_product/test_Product.py
+++ tests/test_product/test_Product.py
@@ -6,15 +6,15 @@ from sales_taxes.product.Product import Product
 class TestProduct(TestCase):
     def test_create_product_from_input(self):
         test_product = Product()
-        test_cases = ["1 a at 1", "1 gum at 1.0", "1 imported gum at 1.1", ""]
-
-        for correct_case in test_cases:
-            test_product.create_product_from_input(correct_case)
-            self.assertEqual(test_product.get_product_amount(), 1, "Should be 1")
 
         test_product.create_product_from_input("1.0 gum at 1")
-        self.assertNotEqual(type(test_product.get_product_amount()), float, "Value shouldn't be a float, should throw exception")
+        self.assertNotEqual(type(test_product.get_product_amount()), float, "Value shouldn't be a float")
+        # and
+        self.assertEqual(test_product.get_product_amount(), 0, "Should be 0, because wrong input")
 
         test_product.create_product_from_input("999 gum at 9.99")
         self.assertEqual(type(test_product.get_product_amount()), int, "Value should be an Integer")
+        self.assertEqual(test_product.get_product_name(), "gum", "Should be string: gum")
+        self.assertEqual(test_product.get_product_price(), 9.99, "Should be float: 9.99")
+        self.assertEqual(type(test_product.get_product_price()), float, "Should be a float")
 
