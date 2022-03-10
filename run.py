c6db23a1246408578972a1c98e5d222bfc882d5e
kamil.o.gorczyca <kamil.o.gorczyca@student.fh-kiel.de>
2022-03-10 23:03:12 +0100

    Finished class ProductIO.py

--- run.py
+++ run.py
@@ -2,15 +2,21 @@
 # Date: 9.3.22
 from sales_taxes.ProductIO import ProductIO
 from sales_taxes.Receipt import Receipt
+from sales_taxes.ShoppingCart import ShoppingCart
 
 
 def main():
     io = ProductIO()
 
     while True:
-        product_string = io.product_input()
-        if check_for_checkout(product_string):
+        checkout_condition = io.product_input()
+        if check_for_checkout(checkout_condition):
+            io.print_receipt()
             break
+
+
+    # To kill python-console
+    exit(0)
 
 
 def check_for_checkout(user_input):
--- sales_taxes/ProductIO.py
+++ sales_taxes/ProductIO.py
@@ -3,26 +3,46 @@ from sales_taxes.ShoppingCart import ShoppingCart
 
 class ProductIO:
     def __init__(self):
-        self.__user_input = ""
-        self.__shoppingCart = ShoppingCart
-
-        # TODO: delete after implementing class Product
-        self.product_amount = 0
-        self.imported = False
-        self.product_name = ""
-        self.product_price = 0.0
+        self.__shopping_cart = ShoppingCart()
         self.TO_LONG_INPUT = 80
         self.TO_SHORT_INPUT = 7
 
-    @staticmethod
-    def product_input():
+    @property
+    def shopping_cart(self):
+        return self.__shopping_cart
+
+    def product_input(self):
         try:
-            return input("\nAdd product to shoppingcart or pay with command 'checkout'\n")
+            user_input = input("\nAdd product to shoppingcart or pay with command 'checkout'\n")
+            if user_input == "checkout":
+                return user_input
+            
+            self.__validate_input(user_input)
         except OverflowError as e:
             print("Error: Invalid argument: {}".format(e))
 
-    def __is_input_valid(self, user_input):
-        return ""
+    def __validate_input(self, user_input):
+        user_input = user_input
+        if self.__is_not_to_long_input(user_input) and self.__is_not_to_short_input(user_input):
+            self.shopping_cart.add_to_cart(user_input)
+        else:
+            self.__print_wrong_user_input("Input to short or to long!")
 
-    def print_receipt(self, receipt):
-        pass
+    def __is_not_to_long_input(self, user_input):
+        return len(user_input) < self.TO_LONG_INPUT
+
+    def __is_not_to_short_input(self, user_input):
+        return len(user_input) > self.TO_SHORT_INPUT
+
+    def __print_wrong_user_input(self, error=None):
+        if error:
+            print("Wrong input, please try again. \nError -> {}".format(error))
+        else:
+            print("Wrong input, please try something like '10 book at 9.99' or '1 imported banana at 1.99'")
+
+    def print_receipt(self):
+        receipt = self.shopping_cart.get_receipt()
+        for product in receipt.get_listed_products():
+            print(product)
+        print("Sales Taxes: {}".format(receipt.get_total_tax()))
+        print("Total: {}".format(receipt.get_total_price()))
