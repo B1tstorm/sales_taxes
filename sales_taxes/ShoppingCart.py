a726afed02721b70495756f186d98851c7acc8b9
kamil.o.gorczyca <kamil.o.gorczyca@student.fh-kiel.de>
2022-03-11 14:20:21 +0100

    Added external module 'switch' for switch statements

--- sales_taxes/ShoppingCart.py
+++ sales_taxes/ShoppingCart.py
@@ -1,15 +1,27 @@
+from switch import Switch
+
 from sales_taxes.Receipt import Receipt
 from sales_taxes.product.Product import Product
 
 
 class ShoppingCart:
     def __init__(self):
-        self.__products = [Product]
+        self.__products = []
         self.__receipt = Receipt()
 
     def add_to_cart(self, product_input):
-        pass
 
+        product = Product()
+        product.create_product_from_input(product_input)
+        self.__products.append(product)
+
+    def __get_instance_by_product_category(self, product_input):
+        category = self.__get_product_category(product_input)
+
+    def __get_product_category(self, product_input):
+
+
+    @property
     def get_receipt(self):
         # TODO: - call __create_receipt()
         return self.__receipt
