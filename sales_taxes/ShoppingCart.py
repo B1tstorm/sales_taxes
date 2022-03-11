2479ddbc106d0c19388d2f54f3116a5f248fcd90
kamil.o.gorczyca <kamil.o.gorczyca@student.fh-kiel.de>
2022-03-11 15:38:23 +0100

    Implemented polymorphism for product categories

--- sales_taxes/ShoppingCart.py
+++ sales_taxes/ShoppingCart.py
@@ -1,6 +1,9 @@
 from switch import Switch
 
 from sales_taxes.Receipt import Receipt
+from sales_taxes.product.Book import Book
+from sales_taxes.product.Food import Food
+from sales_taxes.product.Medical import Medical
 from sales_taxes.product.Product import Product
 
 
@@ -10,16 +13,35 @@ class ShoppingCart:
         self.__receipt = Receipt()
 
     def add_to_cart(self, product_input):
-
-        product = Product()
+        product = self.__get_instance_by_product_category(product_input)
         product.create_product_from_input(product_input)
         self.__products.append(product)
 
     def __get_instance_by_product_category(self, product_input):
         category = self.__get_product_category(product_input)
+        with Switch(category) as case:
+            if case("book"):
+                return Book()
+            if case("pills"):
+                return Medical()
+            if case("food"):
+                return Food()
+            if case.default:
+                return Product()
 
     def __get_product_category(self, product_input):
+        lower_input = product_input.lower()
+        if lower_input.__contains__("book"):
+            print("Its a book")
+            return "book"
+        if lower_input.__contains__("pills"):
+            print("Its medicine")
+            return "medicine"
+        if lower_input.__contains__("food"):
+            print("Its food")
+            return "food"
 
+        return "default"
 
     @property
     def get_receipt(self):
@@ -28,3 +50,7 @@ class ShoppingCart:
 
     def __create_receipt(self, products):
         return [""]
+
+    @property
+    def products(self):
+        return self.__products
--- tests/test_shoppingcart/test_ShoppingCart.py
+++ tests/test_shoppingcart/test_ShoppingCart.py
@@ -2,22 +2,22 @@ from unittest import TestCase
 
 from sales_taxes.Receipt import Receipt
 from sales_taxes.ShoppingCart import ShoppingCart
+from sales_taxes.product.Book import Book
 from sales_taxes.product.Product import Product
 
 
 class TestShoppingCart(TestCase):
     def setUp(self):
         self.test_shopping_cart = ShoppingCart()
-        self.test_products = []
         self.test_receipt: Receipt
 
     def test_add_to_cart(self):
         self.test_shopping_cart.add_to_cart("1 book at 12.49")
-        self.assertIsInstance(self.test_products[0], Product, "Should be instance of Product")
-        self.assertEqual(len(self.test_products), 1, "List should have 1 object")
+        self.assertIsInstance(self.test_shopping_cart.products[0], Book, "Should be instance of Book()")
+        self.assertEqual(len(self.test_shopping_cart.products), 1, "List should have 1 object")
 
         self.test_shopping_cart.add_to_cart(" ")
-        self.assertEqual(len(self.test_products), 1)
+        self.assertEqual(len(self.test_shopping_cart.products), 1)
 
     def test_get_receipt(self):
         self.test_receipt = self.test_shopping_cart.get_receipt()
